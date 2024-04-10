e41bdcb4c25635e1adca61bac53c4978f616ab8e
ackimixs <ackimixs@protonmail.com>
2024-04-10 23:09:35 +0200

    emergency

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include "serialib.h"
+
+#define SERIAL_PORT "/dev/ttyACM0"
+#define BAUDS 115200 //vitesse des données (bit/sec)
+
+serialib init_serial(){
+    serialib serial;
+    char errorOpening = serial.openDevice(SERIAL_PORT, BAUDS);
+    if (errorOpening!=1) exit(errorOpening);
+    return serial;
+}
+
+int main(int argc, char *argv[]) {
+    std::string filePath;
+
+    if (argc == 2) {
+        filePath = argv[1];
+    } else {
+        return 0;
+    }
+
+    std::ifstream file;
+    file.open(filePath);
+
+    std::string line;
+    getline(file, line);
+
+    serialib serial = init_serial();
+
+    std::string toSend = "G " + line + "\n";
+
+    serial.writeBytes(toSend.c_str(), toSend.length());
+
+    serial.closeDevice();
+    file.close();
+
+    return 0;
+}
